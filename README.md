e81f9bb23cedf0a23985e045fe34b8770bb280b0
Ahmed Al-Thawadi <ahmed9althawadi@gmail.com>
2022-10-05 23:19:35 +0300

    Made README.md and Code Cleaning

--- README.md
+++ README.md
@@ -1,1 +1,78 @@
 # Connect Four Game
+
+## App Description
+### The following game goal is to have 4 similar colored circles in color red or blue in either horizontal, vertical, and diagonal line. The red player is user and the blue player a computer.
+
+
+---
+## The Game can be Accessed Through this Link
+### [GitHub Pages Link](https://not9ahmed.github.io/connect-four-game/)
+
+
+---
+## Approach Taken
+Before implementing the project a research was done to find an overall idea of how the game should work and what are the winning conditions for the game. Then, the wireframe was designed to facilitate the design on HTML and CSS. The wireframe was done using Figma tool.
+
+The HTML contained the div with a display of grid, then inside of it there is a 42 divs of circle shape which are organized as 7 columns and 6 row. The divs represent each possible positon possible and it is by default in the color of white. Until a player choose that place. However, the last empty circle in the selected column will be marked. Each game circle has an id that maps to the position in terms of a matrix. So a game circle with id of row-0-col-3 will be mapped to position in the matrix[0][3] and this will simplify the tracking of winning conditions. The matrix will be named as gameTable.
+
+The winning conditions will be as a horizonal, vertical, and diagonal in each possible position. The winning conditions will be checked after each player move. Once a player wins, the scoreboard will increase the number of wins of that player.
+<br>
+
+---
+## Technologies Used
+The project was done using HTML, CSS and JavaScript. For the code editor, I have used VS Code as it offers a lot of functionalites to debug the code. For the wireframe design, Figma deisgn tool was used design all the elements and background colors. In addition, Replit was used relatively to debug some algorithms and functions quickly.
+<br>
+
+---
+## Wireframe
+The below image is the wireframe of the project to be implemented and it was using Figma. Some elements such as the game board, game circles, results div, and scoreboard design was copied from Figma as CSS which helped in lowering the difficultly of designing the elements directly on CSS and HTML.
+<br>
+
+![Tux, the Linux mascot](/Wireframe.jpg)
+
+---
+
+## Post-Project Reflection
+I have found the project simpler after finishing from Tic-Tac-Toe since it helped me to understand the logic and sequencing of games which it was for helpful. The connect four had a similar functionalities it was somewhat tricky, but I eventually understood what was required. I also believe that my design can be improved an I can more cool effect such as animations. Moreoever, I believe that the computer can be more intelligent instead of selecting random places in the connect four board.
+<br>
+
+---
+## Sources Used
+1. [Stack OverFlow Website](https://stackoverflow.com)
+2. [W3Schools Website](https://www.w3schools.com)
+3. [Connect Four Game Website](https://www.mathsisfun.com/games/connect4.html)
+<br>
+
+---
+## HTML Structure
+
+### `title-div`
+The title is mapped into a div called `title-div` this was made to easily center the game title. Then, inside that div it includes an `h1` tag which the title text is in. The title for the game is **Connect Four Game**.  
+<br>
+
+
+### `game-table`  and  `game-circle` 
+For the connect four table it was mapped to a div called `game-table`, then inside that div is a number of circle blocks with the class `game-circle` that will represent each possible position that the players can play. The game circles are the white circle inside the `game-table` div. In addition, each circle has an `id` that represents the corresponding position in terms of a matrix, so for example the first `game-circle` div has an `id` of `row-0-col-1` that corresponds the position in a matrix as `gameTable[0][1]`. The rationale for this is to simplify the DOM manipulation in JavaScript by extracting the circle id and map it easily to a variable called `gameTable` and this variable is a matrix that will map the player positions.  
+<br>
+
+
+### `container`
+The following div contains multiple divs, the first div has an id of `result` and it contains the winner message and by default is set as hidden, so that later on once the winner was found it will be unhidden and will show who won.
+
+Also, inside of that container is div with class of `score-board` which has all the information releated to the number of red wins and blue wins. After each win, the score for the winner player will get updated. If the browser page got refreshed, then the scores will be restarted to 0 for both player.
+
+Lastly, there is a div for the buttons which has a class of `buttons-div` and inside that div is a button with the class of `play-again-btn` that allows the game to be restarted.
+
+
+---
+
+## JavaScript Structure
+<br>
+ 
+
+### Variables Declared
+`gameTable` 
+<br>
+
+
+### Functions Declared
--- script.js
+++ script.js
@@ -1,124 +1,100 @@
-console.log("Hello World")
 
-//Game matrix
+// Accessing the HTML Elements
+
+// getting all the game circles div
 const gameDivs = document.querySelectorAll('.game-circle')
 
-console.log(gameDivs[0])
-
+// getting reset button
 const resetButton = document.querySelector('#play-again-btn')
 
-
+// getting the result div
 const result = document.querySelector('#result')
 
+// game matrix
 let gameTable = [[' ', ' ', ' ', ' ', ' ', ' ', ' '],
-                [' ', ' ', ' ', ' ', ' ', ' ', ' '],
-                [' ', ' ', ' ', ' ', ' ', ' ', ' '],
-                [' ', ' ', ' ', ' ', ' ', ' ', ' '],
-                [' ', ' ', ' ', ' ', ' ', ' ', ' '],
-                [' ', ' ', ' ', ' ', ' ', ' ', ' ']]
+[' ', ' ', ' ', ' ', ' ', ' ', ' '],
+[' ', ' ', ' ', ' ', ' ', ' ', ' '],
+[' ', ' ', ' ', ' ', ' ', ' ', ' '],
+[' ', ' ', ' ', ' ', ' ', ' ', ' '],
+[' ', ' ', ' ', ' ', ' ', ' ', ' ']]
 
-// Setting the value score
+// Setting the wins score for each player and will be initialized to 0
 let redScore = document.getElementById('red-score-value')
 let blueScore = document.getElementById('blue-score-value')
-
-
-
 redScore.innerText = '0'
 blueScore.innerText = '0'
 
 
-//Variable to stop the game
+// variable to stop the game
 let gameStop = false
 
-//Variable to store the winner
+// variable to store the winner
 let winner
 
-//store the winner score after each win increase the counter
+// store the number of win for each player
 let redWins = 0
 let blueWins = 0
 
-
+// variable to store the number of plays played
 let playsCount = 0
 
-
+// variable to check if the player selected circle is not empty
 let playerColumnEmpty = true
 
-//Function to print our the winner
+// function to print out the winner in the HTML
 const printWinner = () => {
 
-
+    // Displaying the result div
     result.classList.remove('hide-winner')
 
-
+    // Printing out the winner
     let winnerArr = winner.split('')
-
     let upperFirst = winnerArr[0].toUpperCase()
     let restLetters = winnerArr.splice(1).join('')
-
-
-
     let captializedWord = upperFirst.concat(restLetters)
     result.innerText = `${captializedWord} Won`
 
-    console.log(result)
-
-    console.log(`The winner is ${winner}`)
 }
 
 
 
-//function to increase the winnerCount
+// Function to increase the wins count for each player
 const increaseWinsCount = (player) => {
-
-    if(player === 'red'){
+    if (player === 'red') {
         redWins++
         redScore.innerText = String(redWins)
-
-    } else if(player === 'blue') {
+    } else if (player === 'blue') {
         blueWins++
         blueScore.innerText = String(blueWins)
-
     }
-
 }
 
-
+// Function to disable all the gameCircles div
 const disableDivs = () => {
-    for(let i=0; i<42; i++){
+    for (let i = 0; i < 42; i++) {
         divs[i].classList.add('disable')
     }
 }
 
 
-//function to check the winner
+// Function to check the winner with theree conditions
 const checkWinner = (player) => {
 
-
-
-    //Horizontal Line
+    //Checking for Horizontal Line '-'
     for (let row = 0; row < 6; row++) {
-
         for (let column = 0; column < 4; column++) {
 
             let secondColumn = column + 1
             let thirdColumn = column + 2
             let fourthColumn = column + 3
 
-
-
-
             if (gameTable[row][column] !== ' ' && gameTable[row][column] === gameTable[row][secondColumn]
                 && gameTable[row][secondColumn] === gameTable[row][thirdColumn] && gameTable[row][thirdColumn] === gameTable[row][fourthColumn]) {
 
                 console.log("Horizontal Line")
-
-                //   console.log(`${gameTable[row][column]} Won`)
                 winner = gameTable[row][column]
-
                 printWinner()
-
                 increaseWinsCount(player)
-
                 gameStop = true
                 break;
             }
@@ -127,92 +103,65 @@ const checkWinner = (player) => {
     }
 
 
-    //Vertical Line
+    //Checking for Vertical Line '|'
     for (let row = 0; row < 3; row++) {
-        
         for (let column = 0; column < 7; column++) {
-            
+
             let secondRow = row + 1
             let thirdRow = row + 2
             let fourthRow = row + 3
-            
+
             if (gameTable[row][column] !== ' ' && gameTable[row][column] === gameTable[secondRow][column]
-            && gameTable[secondRow][column] === gameTable[thirdRow][column] && gameTable[thirdRow][column] === gameTable[fourthRow][column]) {
-                
+                && gameTable[secondRow][column] === gameTable[thirdRow][column] && gameTable[thirdRow][column] === gameTable[fourthRow][column]) {
+
                 console.log("Vertical Line")
-                
-                //   console.log(`${gameTable[row][column]} Won`)
-                
-                
-                //no need I think
                 winner = gameTable[row][column]
-                
                 printWinner()
-
-
                 increaseWinsCount(player)
-                
                 gameStop = true
                 break;
             }
         }
-        
     }
-    
 
-    // diagonal line
 
-    for(let row=0; row<3; row++){
-        for(let column = 0; column<4; column++){
-            
+    // Checking for Diagonal line '\'
+    for (let row = 0; row < 3; row++) {
+        for (let column = 0; column < 4; column++) {
+
             let secondRow = row + 1
             let secondColumn = column + 1
 
-            
             let thirdRow = row + 2
             let thirdColumn = column + 2
-
 
             let fourthRow = row + 3
             let fourthColumn = column + 3
 
-
-            // diagonal line
-            if(gameTable[row][column] !== ' ' && gameTable[row][column] === gameTable[secondRow][secondColumn]
-            && gameTable[secondRow][secondColumn] === gameTable[thirdRow][thirdColumn] && gameTable[thirdRow][thirdColumn] === gameTable[fourthRow][fourthColumn]){
-                
-
+            if (gameTable[row][column] !== ' ' && gameTable[row][column] === gameTable[secondRow][secondColumn]
+                && gameTable[secondRow][secondColumn] === gameTable[thirdRow][thirdColumn] && gameTable[thirdRow][thirdColumn] === gameTable[fourthRow][fourthColumn]) {
 
                 console.log("Diagonal Line")
-                
-                //   console.log(`${gameTable[row][column]} Won`)
-                
-                
-                //no need I think
+
                 winner = gameTable[row][column]
-                
                 printWinner()
-
-
                 increaseWinsCount(player)
-                
                 gameStop = true
                 break;
             }
-
         }
     }
 
 
-    // anti diagonal
-    for(let row=5; row>2; row--){
-        for(let column = 0; column<4; column++){
+    // Anti Diagonal '/'
+    for (let row = 5; row > 2; row--) {
+        for (let column = 0; column < 4; column++) {
 
 
             let secondRow = row - 1
             let secondColumn = column + 1
 
-            
+
             let thirdRow = row - 2
             let thirdColumn = column + 2
 
@@ -221,57 +170,39 @@ const checkWinner = (player) => {
             let fourthColumn = column + 3
 
 
-            if(gameTable[row][column] !== ' ' && gameTable[row][column] === gameTable[secondRow][secondColumn]
-            && gameTable[secondRow][secondColumn] === gameTable[thirdRow][thirdColumn] && gameTable[thirdRow][thirdColumn] === gameTable[fourthRow][fourthColumn]){
-                
+            if (gameTable[row][column] !== ' ' && gameTable[row][column] === gameTable[secondRow][secondColumn]
+                && gameTable[secondRow][secondColumn] === gameTable[thirdRow][thirdColumn] && gameTable[thirdRow][thirdColumn] === gameTable[fourthRow][fourthColumn]) {
 
                 console.log("Anti Diagonal Line")
-                
 
                 winner = gameTable[row][column]
-                
                 printWinner()
-
-
                 increaseWinsCount(player)
-                
                 gameStop = true
                 break;
             }
 
         }
-
     }
 
-    //increase the plays count
+    //increase the plays count which helps to find a tie
     playsCount++
     console.log(playsCount)
 
-    //check tie
-    if(playsCount === 42){
-
-
+    // Checking for tie
+    if (playsCount === 42) {
         result.classList.remove('hide-winner')
         result.innerText = `It is a tie!!`
-    
-    
         console.log(`It is a tie!!`)
-
         gameStop = true
     }
-    
-    
-    // console.log(`The winner is..${winner}`)
 }
 
 
 
-
-
-
-//function to insert elements inside each square
+// function to insert elements inside each square
 const insertCircle = (divID, player) => {
-    
+
 
     console.log(divID)
 
@@ -279,120 +210,101 @@ const insertCircle = (divID, player) => {
 
     console.log(divToBeInserted)
 
-    if(player === 'red'){
+    if (player === 'red') {
         divToBeInserted.classList.add('red-circle-img')
     } else {
         divToBeInserted.classList.add('blue-circle-img')
     }
-
-
-
-
 }
 
 
-
-
-//function to make the computer make a move
+// function to make the computer make a move
 const computerPlays = () => {
 
     //Variable to store the selected div
     let divSelected
-    
-    
-    
+
     let emptyColNotFound = true
-    
     let randomColumn
-    
-    while(emptyColNotFound){
-        
-        randomColumn = Math.floor(Math.random()*7)
-        
+
+
+    //Generating a random column that have spaces to add the computer positon
+    while (emptyColNotFound) {
+
+        // Generate random column between 0 and 7
+        randomColumn = Math.floor(Math.random() * 7)
+
         //check if the row is empty or not
-        //can be a function
-        for(let row=0; row<6; row++){
-            
-            if(gameTable[row][randomColumn] === ' '){
-                
-                
+        // and insert the blue position in the empty column
+        for (let row = 5; row >= 0; row--) {
+
+            if (gameTable[row][randomColumn] === ' ' &&
+                (gameTable[row][randomColumn] !== 'red' || gameTable[row][randomColumn] !== 'blue')) {
+
+                gameTable[row][randomColumn] = 'blue'
+
+                // row-5-col-0
+                divSelected = `row-${row}-col-${randomColumn}`
+
+
                 emptyColNotFound = false
+                console.log('row: ' + row)
+
+
                 break
+
             }
         }
-        
     }
-    
-    console.log(randomColumn)
-    
-    
-    for(let row=5; row>=0; row--){
-        
-        if(gameTable[row][randomColumn] === ' ' &&
-        (gameTable[row][randomColumn] !== 'red' || gameTable[row][randomColumn] !== 'blue')){
-            
-            gameTable[row][randomColumn] = 'blue'
-            
-            // row-5-col-0
-            divSelected = `row-${row}-col-${randomColumn}`
-            
-            break
-            
-        }
-    }
-    
-    
+
+    console.log('col: ' + randomColumn)
+
     console.log('Computer Plays')
     console.log(gameTable)
-    
-    
+
+
     //insert to the html
     insertCircle(divSelected, 'blue')
-    
+
     // Must end the game
     checkWinner('blue')
-    
+
 }
 
 // function to disable the column selected
 const disableColumn = (columnToDisable) => {
-    
-    for(let row = 0; row<6; row++){
-        
+
+    for (let row = 0; row < 6; row++) {
+
         let divID = `row-${row}-col-${columnToDisable}`
-        
+
         let div = document.getElementById(divID)
-        
+
         div.classList.add('disable')
     }
-} 
+}
 
 
-//function to check if the column is not empty
+// function to check if the column is not empty
+//return true if the column empty. Othewise, return false
 const checkColumnEmpty = (columnToSearch) => {
-    for(let row=5; row>=0; row--){
-        if(gameTable[row][columnToSearch] === ' '){
+    for (let row = 5; row >= 0; row--) {
+        if (gameTable[row][columnToSearch] === ' ') {
             return true
         }
     }
-
     return false
 }
 
 
-//function to make the player make a move
+// function to make the player make a move. It will first take the
+// clicked div id as a parameter and will make a move
+// by checking which div clicked on and find the last empty div in that
+// selected div column
 const playerPlays = (clickedDivID) => {
-    
-    
-    // let clickedDiv = document.getElementById(clickedDivID)
-    
-    console.log(clickedDivID)
-    
+
 
     let arr = clickedDivID.split('-')
-
-    console.log(arr)
-
     let row = parseInt(arr[1])
     let col = parseInt(arr[3])
 
@@ -402,15 +314,13 @@ const playerPlays = (clickedDivID) => {
     let divSelected
 
 
-
+    // First check if the clicked on div is not empty if it is empty
+    // if it is not empty then return false and don't insert anything
     let columnEmpty = checkColumnEmpty(col)
 
-    if(columnEmpty === false){
-
-
-        //
+    if (columnEmpty === false) {
+        // call function to remove disable effect
         disableColumn(col)
-
 
         //return that the column is not empty
         return columnEmpty
@@ -419,45 +329,33 @@ const playerPlays = (clickedDivID) => {
 
     console.log(columnEmpty)
 
-    //It must find an empty column
+    // It must find an empty column
     // Mark the choosen place in  the gameTable Matrix
-    for(let row=5; row>=0; row--){
+    for (let row = 5; row >= 0; row--) {
 
-        if(gameTable[row][col] === ' ' &&
-        (gameTable[row][col] !== 'red' || gameTable[row][col] !== 'blue')){
+        if (gameTable[row][col] === ' ' &&
+            (gameTable[row][col] !== 'red' || gameTable[row][col] !== 'blue')) {
 
+            // add the player position in the game table at the last row
             gameTable[row][col] = 'red'
-            
-
-            // row-5-col-0
             divSelected = `row-${row}-col-${col}`
-
             break
         }
     }
 
-
-    // playsCount++
-
-
     console.log('Player Plays')
     console.log(gameTable)
-
 
     //insert to the html
     insertCircle(divSelected, 'red')
 
-
     // Must check the game end the game
     checkWinner('red')
-
-
-
 }
 
 
-// function to to disable game/circle
-const disableButton = () => {
+// function to to disable game circle
+const disableHover = () => {
     //Remove the click effect
     for (let i = 0; i < gameDivs.length; i++) {
         gameDivs[i].classList.add('disable')
@@ -465,95 +363,70 @@ const disableButton = () => {
 }
 
 
-
-
 //function to set each gamecircle
 const setGameCircle = (event) => {
 
 
-    //can be a function
-    if(gameStop){
-
-        disableButton()
-
+    // if the game stopped then don't do anything
+    if (gameStop) {
+        disableHover()
         console.log('Game Ended')
         return
     }
-    
+
     // get the id of clicked element
     let clickedDivID = event.target.id
-    
-    
-    console.log(clickedDivID)
-
-
-    // must make sure that the clicked element is a div
-    // will be false if any other element was clicked on
-    if(!clickedDivID.includes('col')){
-        return
-    }
-
 
     //make player play and if the column is not empty then don't allow him to play
     let playerColumnEmpty = playerPlays(clickedDivID)
 
-    if(playerColumnEmpty === false){
-
+    if (playerColumnEmpty === false) {
         return
     }
 
-
-    //can be a function
-    if(gameStop){
-
-        disableButton()
-
+    // if the player won then stop the game
+    if (gameStop) {
+        disableHover()
         console.log('Game Ended')
         return
     }
-
 
     //make computer play with some delay
     setTimeout(computerPlays, 100)
 
-    //can be a function
-    if(gameStop){
-
-
-        disableButton()
-          
+    // if the computer won then stop the game
+    if (gameStop) {
+        disableHover()
         console.log('Game Ended')
         return
     }
-
-
 }
 
-
+// Function to set the game table and it will add event listen to each
+// game circle
 const setGameTable = () => {
     console.log('table is being set')
+    for (let i = 0; i < gameDivs.length; i++) {
 
-    for(let i=0; i<gameDivs.length; i++){
-
-        document.addEventListener('click', setGameCircle)
-
+        // CHECK THIS AGAIN
+        // document.addEventListener('click', setGameCircle) //BEFORE
+        gameDivs[i].addEventListener('click', setGameCircle)
     }
 }
 
-
+// function to reset the game table and it will be run once a user
+// clicked on the Play Again button
 const resetGameTable = () => {
 
+    // Resetting the variables
+    gameStop = false
+    playsCount = 0
+    winner = ''
     playerColumnEmpty = true
 
-    gameStop = false
-
-    playsCount = 0
-
-    winner = ''
-
-    for(let row=0; row<6; row++){
-
-        for(let column=0; column<7; column++){
+    // resetting the game table matrix
+    for (let row = 0; row < 6; row++) {
+        for (let column = 0; column < 7; column++) {
 
             //restart the gameTable
             gameTable[row][column] = ' '
@@ -562,28 +435,24 @@ const resetGameTable = () => {
         }
     }
 
-    // i<42
-    for(let i =0; i<gameDivs.length; i++){
+    // Resetting the class for the divs and removing the disable effect
+    for (let i = 0; i < gameDivs.length; i++) {
         gameDivs[i].className = 'game-circle'
         gameDivs[i].classList.remove('disable')
     }
 
 
+    // hide the winner div
     result.classList.add('hide-winner')
-
-
-    console.log(gameTable)
-
 
     //Set game table again
     setGameTable()
 }
 
 document.addEventListener('DOMContentLoaded', () => {
+    //Set the game table
     setGameTable()
 
     //clear all the divs using the reset button
     resetButton.addEventListener('click', resetGameTable)
-
-
 })
